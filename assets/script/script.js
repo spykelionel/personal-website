a7dd090a642c838d9b5957d912dd04f589ac1755
spykelionel <social.lionel@gmail.com>
2024-07-01 15:09:28 +0100

    add skill

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,15 @@
 const goals = [
   {
-    title: "",
-    icon: "",
-    description: "",
+    title: "Learn React",
+    description: "Study Modern Frontend Development with React",
+  },
+  {
+    title: "Learn React",
+    description: "Study Modern Frontend Development with React",
+  },
+  {
+    title: "Learn React",
+    description: "Study Modern Frontend Development with React",
   },
 ];
 
@@ -37,3 +44,26 @@ function updateTime() {
 // Update time every second
 setInterval(updateTime, 1000);
 updateTime();
+
+const Goal = ({
+  id,
+  title,
+  description,
+}) => `<div class="flex justify-between items-center flex-col gap-2 border border-solid">
+              <p
+                class="rounded-full p-2 border border-solid border-primary w+8 h+8 text-center"
+              >
+                ${id}
+              </p>
+              <h3>${title}</h3>
+              <p>${description}</p>
+            </div>`;
+
+function InjectGoals(goals) {
+  const goalsRef = document.getElementById("goals");
+  goals.map((goal, id) => {
+    goalsRef.innerHTML = goalsRef.innerHTML + Goal({ ...goal, id: id + 1 });
+  });
+}
+
+InjectGoals(goals);
